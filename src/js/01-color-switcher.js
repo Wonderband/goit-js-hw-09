e43a515df17649ef60e9bec5fe5cd6928dbd39b1
Alex Koniushenko <alex.koniushenko@gmail.com>
2022-11-10 23:13:03 +0200

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+  
+const bodyEl = document.querySelector('body');
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+btnStartEl.addEventListener('click', changeBodyColors);
+btnStopEl.addEventListener('click', stopChangeBodyColors);
+btnStopEl.disabled = true;
+
+function changeBodyColors() {
+    btnStartEl.disabled = true;  
+    btnStopEl.disabled = false;  
+    intervalId = setInterval(() => bodyEl.style.backgroundColor = getRandomHexColor(), 1000);    
+}
+
+function stopChangeBodyColors() {
+    clearInterval(intervalId);
+    btnStartEl.disabled = false;
+    btnStopEl.disabled = true;  
+}
